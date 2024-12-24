1b262a74dbabaca72617c8c82a660f08401f5baa
yosrfarouk <spacesusu@gmail.com>
2024-12-24 12:17:07 +0300

    homework

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'translations.dart';
+import 'login_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      translations: MyTranslations(), // إضافة الترجمة
+      locale: Locale('en'), // اللغة الافتراضية
+      fallbackLocale: Locale('en'), // لغة احتياطية
+      theme: ThemeData.light(), // الوضع المضيء
+      darkTheme: ThemeData.dark(), // الوضع المعتم
+      themeMode: ThemeMode.system, // الوضع الافتراضي حسب النظام
+      home: LoginScreen(),
+    );
+  }
+}
+
